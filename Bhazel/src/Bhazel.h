c3be021d06496e89088dbd3027509cbec99c2df5
Bruno Lourenço <madequa@users.noreply.github.com>
2019-03-18 01:21:50 +0000

    Key and Mouse Codes video.

--- Bhazel/src/Bhazel.h
+++ Bhazel/src/Bhazel.h
@@ -3,8 +3,14 @@
 //Convenience header for use by Bhazel client applications
 
 #include "Bhazel/Application.h"
+
+#include "Bhazel/Input.h"
+#include "Bhazel/KeyCodes.h"
+#include "Bhazel/MouseButtonCodes.h"
+
 #include "Bhazel/Layer.h"
 #include "Bhazel/Log.h"
+
 #include "Bhazel/ImGui/ImGuiLayer.h"
 
 
